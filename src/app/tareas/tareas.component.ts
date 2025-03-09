222e323c21679d959df90c4184d636edee66d05b
juanakan <36744896+juanakan@users.noreply.github.com>
2025-03-09 01:05:15 +0100

    formulario tareas

--- src/app/tareas/tareas.component.ts
+++ src/app/tareas/tareas.component.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 import { TareasService } from '../services/tareas.service';
 import { Task } from '../models/task.model';
 import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tareas',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, FormsModule],
   templateUrl: './tareas.component.html',
   styleUrl: './tareas.component.css'
 })
 export class TareasComponent {
 
   tareas: Task[]=[];
+  nuevaTarea: Task = { id: 0, titulo: '', description: '', completado: false, userId: 1 };
   constructor(private tareasService: TareasService){}
   ngOnInit() {
     this.tareasService.obtenerTareas().subscribe({
@@ -24,4 +26,6 @@ export class TareasComponent {
       }
     });
   }
+
+  agregarTarea(){}
 }
